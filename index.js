d0fa5e777487ec0b8667cd194e2dedddc5784379
Aaron G Meininger <a.g.meininger@gmail.com>
2018-05-04 15:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function happyHolidays() {
   return ("Happy holidays!")
 }
 
-function happyHolidaysTo(firstName) {
-  
+function happyHolidaysTo(name) {
+  return ("Happy holidays ${name}!")
 }
+
+function happyHolidayTo()
